d98c597c96b250562a952b52726a09b9d1edabbc
Poom <68049473+poompongphun@users.noreply.github.com>
2022-08-23 21:41:48 +0700

    add login page

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,5 @@
+<div class="m-5">
+	<div class="text-3xl font-bold py-1">โปรดเข้าสู่ระบบ</div>
+	<div class="text-lg text-[#616573]">ใช้อีเมลสถาบันในการเข้าสู่ระบบ</div>
+</div>
+<div class="h-[375px] bg-[#F3F7FA] flex justify-center items-center">Google Button</div>
