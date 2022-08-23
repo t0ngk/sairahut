0757be16ab015fe0c4550abc935e015585be5d50
Poom <68049473+poompongphun@users.noreply.github.com>
2022-08-24 00:18:20 +0700

    feat: 🎸 add collection page

--- src/routes/collection/+page.svelte
+++ src/routes/collection/+page.svelte
@@ -1,1 +1,21 @@
-<div>collection</div>
+<div class="m-5">
+	<div class="text-3xl font-bold py-1 flex mb-2">
+		<span class="border-b-2 border-[#f20c32] mr-2">Pokedex collection</span>
+	</div>
+	<div class="text-md text-[#616573]">Everyone’s pokemons will display here</div>
+
+	<div class="grid grid-cols-2 sm:grid-cols-3 gap-2 mt-5">
+		<a
+			class="w-full aspect-square border-2 border-[#B0CAF4] bg-[#F3F7FA] rounded-2xl flex justify-center items-center relative"
+            href="/collection/1"
+		>
+			<div class="w-1/2 h-1/2 bg-[#E5EEF4] rounded-full relative flex justify-center items-center">
+				<img
+					class="w-full"
+					src="https://th.portal-pokemon.com/play/resources/pokedex/img/pm/5794f0251b1180998d72d1f8568239620ff5279c.png"
+					alt="pokemon"
+				/>
+			</div>
+		</a>
+	</div>
+</div>
---
+++ src/routes/collection/[slug]/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { page } from '$app/stores';
+	import Pokedex from '../../components/pokedex.svelte';
+	const slug = $page.params.slug;
+</script>
+
+<Pokedex
+	data={{
+		name: 'Zenigame',
+		image: 'https://th.portal-pokemon.com/play/resources/pokedex/img/pm/5794f0251b1180998d72d1f8568239620ff5279c.png',
+		element: 'water',
+		lvl: 24,
+		hp: { remain: 60, max: 100 },
+		def: 35,
+		atk: 50,
+		hints: ['หล่อเท่สุด', 'เหล่ท่อสุด']
+	}}
+/>
