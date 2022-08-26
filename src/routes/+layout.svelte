d32cef489518567ccb081aee63ca34c581bf1e30
C0M3TZZ <39180519+C0M3TZZ@users.noreply.github.com>
2022-08-26 20:19:29 +0700

    :sparkles: : Add admin page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,18 @@
 	import '../app.postcss';
 	import Navbar from './components/navbar.svelte';
 	import SnackBar from './components/SnackBar.svelte';
+	import NavbarAdmin from './components/navbarAdmin.svelte';
+	import { page } from '$app/stores';
 </script>
 
-<SnackBar />
-<div class="mx-auto max-w-[824px]">
-	<Navbar />
+{#if $page.url.pathname !== '/admin'}
+	<SnackBar />
+	<div class="mx-auto max-w-[824px]">
+		<Navbar />
+		<slot />
+	</div>
+{:else}
+	<SnackBar />
+	<NavbarAdmin />
 	<slot />
-</div>
+{/if}
