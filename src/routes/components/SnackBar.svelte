93788ef3f4da9c91da0e185df63be56e69ceca6c
Poom <68049473+poompongphun@users.noreply.github.com>
2022-08-28 01:42:00 +0700

    style: 💄 update z val

--- src/routes/components/SnackBar.svelte
+++ src/routes/components/SnackBar.svelte
@@ -29,7 +29,7 @@
 	});
 </script>
 
-<div class="fixed w-full z-10 top-0 h-fit pointer-events-none">
+<div class="fixed w-full z-50 top-0 h-fit pointer-events-none">
 	<div class="w-full max-w-[824px] h-full p-3 mx-auto">
 		<div
 			class="w-full pointer-events-auto bg-[rgba(221,221,221,0.3)] backdrop-blur-sm rounded-lg p-3 overflow-hidden {show
