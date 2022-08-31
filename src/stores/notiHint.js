d5dca141460277addf93ea8c7127ceff01bc23e8
C0M3TZZ <39180519+C0M3TZZ@users.noreply.github.com>
2022-08-31 19:41:57 +0700

    :rocket: : Test Deploy #1

--- src/stores/notiHint.js
+++ src/stores/notiHint.js
@@ -1,5 +1,5 @@
 import { writable } from 'svelte/store';
-import { browser } from '$app/env';
+import { browser } from '$app/environment';
 import notification from './notification';
 
 // const stored = JSON.parse(localStorage.getItem('hints'));
