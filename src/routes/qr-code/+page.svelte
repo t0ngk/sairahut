4ccb3ccebb5611e69b83ceeb3152a64d36a85492
C0M3TZZ <ktong254@gmail.com>
2022-09-07 20:28:59 +0700

    Revert ":sparkles: : QR-Code Scanner to Catch Pokemon!!"
    This reverts commit dc880fd6a7f0f84a62a03f410d3e490cf1b9334a.

---
+++ src/routes/qr-code/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+	let videoElement = '';
+	import QrScanner from 'qr-scanner';
+	import { onMount } from 'svelte';
+
+	onMount(async () => {
+		const qrScanner = new QrScanner(videoElement, (result) => {
+      alert(result);
+		});
+
+		qrScanner.start();
+	});
+</script>
+
+<div class="mx-auto">
+	<video class="rounded" bind:this={videoElement} src="">
+		<track kind="captions" />
+	</video>
+</div>
