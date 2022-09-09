f3366e1e3c91f9b039ec76ee26cddd95f3ef5892
Poom <68049473+poompongphun@users.noreply.github.com>
2022-09-09 14:50:31 +0700

    style: 💄 add loading sak noi la gunn

--- src/routes/qr-code/+page.svelte
+++ src/routes/qr-code/+page.svelte
@@ -16,6 +16,7 @@
 
 	let qrCodeElement = '';
 	let QrImg = '';
+	let loading = true;
 	const generateQR = async (text) => {
 		try {
 			return await QRCode.toCanvas(qrCodeElement, text, {
@@ -32,6 +33,7 @@
 	};
 
 	onMount(async () => {
+		loading = true;
 		const qrCodeUrl = await get('/api/auth/profile/qrcode', window.localStorage.getItem('token'));
 		if (qrCodeUrl.status == 400) {
 			window.location.href = '/';
@@ -40,15 +42,34 @@
 		const qr = await generateQR(qrCodeUrl);
 		let dataUrl = qr.toDataURL();
 		QrImg = dataUrl;
+		loading = false;
 	});
 </script>
 
 <!-- <canvas class="mx-auto" bind:this={qrCodeElement} /> -->
 <div class="flex justify-center items-center" style="height: calc(100vh - 98px)">
-	<div class="bg-[#f3f7fa] p-5 rounded-xl mx-4 border-4 border-[#B0CAF4]" style="max-width: 500px;">
-		<img src={QrImg} alt="qrcode" width="100%" />
+	<div
+		class="bg-[#f3f7fa] p-5 rounded-xl mx-4 border-4 border-[#B0CAF4] relative w-full"
+		style="max-width: 500px;"
+	>
+		{#if !loading}
+			<img src={QrImg} alt="qrcode" width="100%" />
+		{:else}
+			<div class="aspect-square w-full flex justify-center items-center">
+				<div class="w-[15%] bg-white aspect-square relative rounded-full overflow-hidden">
+					<div class="absolute animate-spin w-full h-full flex justify-center items-center">
+						<div class="w-full h-[40%] rounded-full bg-[#B0CAF4] aspect-square absolute bottom-0" />
+					</div>
+					<div
+						class="bg-[#f3f7fa] aspect-square rounded-full absolute"
+						style="width: 75%; top: calc(50% - (75% / 2)); left: calc(50% - (75% / 2))"
+					/>
+				</div>
+			</div>
+		{/if}
 		<div class="mt-4 mb-1 text-4xl font-black text-[#111C43] flex justify-center items-center">
-			<img class="mr-2" src="/icons/pokeball.png" style="width: 30px; height: 30px" alt="poke" /> Catch me
+			<img class="mr-2" src="/icons/pokeball.png" style="width: 30px; height: 30px" alt="poke" /> Catch
+			me
 		</div>
 	</div>
 </div>
